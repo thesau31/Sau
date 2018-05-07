134555dbfc0d56beb3cd475260e8aa46bfd6433f
Scott <ssau31@gmail.com>
2018-05-07 10:59:46 -0500

    Added DiceBag

---
+++ Sau.Raylan.Tests/DiceBagTests.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sau.Raylan.Tests
+{
+    [TestClass]
+    public class DiceBagTests
+    {
+        [TestClass]
+        public class d6
+        {
+            [TestMethod]
+            public void GivenNegativeTimes_ThenEmptyResults()
+            {
+                // arrange
+                var bag = new DiceBag();
+                var expected = 0;
+
+                // act
+                var results = bag.d6(-1);
+
+                // assert
+                Assert.AreEqual(expected, results.Count());
+            }
+
+            [TestMethod]
+            public void GivenZeroTimes_ThenEmptyResults()
+            {
+                // arrange
+                var bag = new DiceBag();
+                var expected = 0;
+
+                // act
+                var results = bag.d6(0);
+
+                // assert
+                Assert.AreEqual(expected, results.Count());
+            }
+
+            [TestMethod]
+            public void d6_Valid()
+            {
+                // arrange
+                const int seed = 1234;
+                var bag = new DiceBag(seed);
+                var expected = new Random(seed).Next(1, 6);
+
+                // act
+                var results = bag.d6();
+
+                // assert
+                Assert.AreEqual(expected, results);
+            }
+
+            [TestMethod]
+            public void d6_Valid_List()
+            {
+                // arrange
+                const int seed = 1234;
+                var bag = new DiceBag(seed);
+                var random = new Random(seed);
+                var expected = new List<int>
+            {
+                random.Next(1, 6),
+                random.Next(1, 6),
+                random.Next(1, 6),
+                random.Next(1, 6)
+            };
+
+                // act
+                var results = bag.d6(4).ToList();
+
+                // assert
+                Assert.AreEqual(expected.Count, results.Count);
+                for (int i = 0; i < results.Count; i++)
+                    Assert.AreEqual(expected[i], results[i]);
+            }
+
+            [TestMethod]
+            public void d6_ResultsAreWithinRange()
+            {
+                // arrange
+                var bag = new DiceBag();
+
+                // act 
+                var results = bag.d6(10000);
+
+                // assert
+                Assert.AreEqual(0, results.Count(x => x < 1));
+                Assert.AreEqual(0, results.Count(x => x > 6));
+            }
+        }
+    }
+}
---
+++ Sau.Raylan/DiceBag.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sau.Raylan
+{
+    public class DiceBag
+    {
+        private Random _random;
+
+        #region c'tor
+        public DiceBag()
+        {
+            _random = new Random();
+        }
+
+        public DiceBag(int seed)
+        {
+            _random = new Random(seed);
+        }
+        #endregion
+
+        #region d6
+        public int d6()
+        {
+            return roll(6);
+        }
+
+        public IEnumerable<int> d6(int times)
+        {
+            for (int i = 0; i < times; i++)
+                yield return roll(6);
+        } 
+        #endregion
+
+        private int roll(int sides)
+        {
+            return _random.Next(1, sides);
+        }
+    }
+}
